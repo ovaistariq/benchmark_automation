f903f7f954bf5747176d0bc6625afe249dedbcef
Fernando Ipar <fipar@acm.org>
2015-12-24 15:45:54 -0300

    added script to generate a csv file from sysbench output

---
+++ data_preparation_scripts/csv_from_sysbench.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# converts these lines of sysbench output: 
+# [  10s] threads: 16, tps: 474.30, reads: 6653.36, writes: 1897.92, response time: 39.34ms (95%), errors: 0.00, reconnects:  0.00
+# to these: 
+# oltp,10000,16,10,902.58,139.75,224.90
+
+[[ $# -ne 3 || ! -r $1 ]] && {
+cat <<EOF>&2
+  Error: missing or invalid arguments: ($*)
+  usage: $0 <file> <bench> <size>
+    where: 
+     - <file> is the filename to a sysbench output file
+     - <bench> is the test name (i.e. oltp, update_index, etc)
+     - <size> is the table size in some multiple of rows (usually 1k)
+
+EOF
+exit 1
+}
+
+file=$1; bench=$2; size=$3
+
+echo "workload,size,threads,ts,writes,reads,response_time,tps"
+
+cat $file | awk -F ',' '{
+	f1=""
+        for(i=1; i<=NF; i++) {
+                tmp=match($i, /\[[[:space:]]*(.*)s\]/,a)
+                if(tmp) {
+                        ts=a[1]
+                }
+                tmp=match($i, /[[:space:]]*writes:[[:space:]]+(.*)/,b)
+                if(tmp) {
+                        writes=b[1]
+                }
+                tmp=match($i, /[[:space:]]*reads:[[:space:]]+(.*)/,b)
+                if(tmp) {
+                        reads=b[1]
+                }
+                tmp=match($i, /[[:space:]]*response time:[[:space:]]+(.*)ms/,b)
+                if(tmp) {
+                        response_time=b[1]
+                }
+                tmp=match($i, /[[:space:]]*tps:[[:space:]]+(.*)/,b)
+                if(tmp) {
+                        tps=b[1]
+                }
+                tmp=match($i, /[[:space:]]*threads:[[:space:]]+(.*)/,b)
+                if(tmp) {
+                        threads=b[1]
+                }
+        }
+	if (ts)
+	print bench","size","threads","ts","writes","reads","response_time","tps
+}' bench=$bench size=$size 
