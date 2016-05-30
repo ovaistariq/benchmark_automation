af8d457ec43609ae3d988e021fea6f8cb1e1f716
Fernando Ipar <fipar@acm.org>
2016-05-30 19:59:26 -0300

    added script to transform dstat output into proper csv

---
+++ data_preparation_scripts/cleanup_dstat.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+[ $# -eq 0 ] && {
+    echo "usage: $0 <file>">&2
+    exit 1
+}
+
+fn="$1"
+
+sed -n '/^$/,//p' < "$fn" | grep -v ^$ | (
+	i=0
+	while read l; do
+	    [ $i -eq 0 ] && echo -n "$l" || echo $l
+	    i=$((i+1))
+	done
+)
+
