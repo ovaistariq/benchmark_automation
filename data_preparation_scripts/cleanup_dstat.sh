0a38ced0bb63c7c66e89dd9a147f99e03bc28155
Fernando Ipar <fipar@acm.org>
2016-06-03 22:11:40 -0300

    added support to print only the header, and without the header, for concating several files

--- data_preparation_scripts/cleanup_dstat.sh
+++ data_preparation_scripts/cleanup_dstat.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
 
 [ $# -eq 0 ] && {
+    cat <<EOF>&2
+   usage: $0 <file>
+Optionally set one of the following env variables: 
+   _NOHEADER (do not print the header line)
+   _ONLYHEADER (only print the header line)
+EOF
     echo "usage: $0 <file>">&2
     exit 1
 }
 
 fn="$1"
+printheader=1
+onlyheader=0
+[ -n "$_NOHEADER" ] && printheader=0
+[ -n "$_ONLYHEADER" ] && onlyheader=1
 
 sed -n '/^$/,//p' < "$fn" | grep -v ^$ | (
 	i=0
 	while read l; do
-	    [ $i -eq 0 ] && echo -n "i,$l" || echo "$i,$l"
+	    if [ $i -eq 0 ]; then
+	       [ $printheader -eq 1 ] && echo -n "i,$l"
+	    elif [ $i -eq 1 ]; then
+		[ $printheader -eq 1 ] && echo "$i,$l"
+	    else
+		[ $onlyheader -eq 1 ] && break
+		echo "$i,$l"
+	    fi
 	    i=$((i+1))
 	done
 )
