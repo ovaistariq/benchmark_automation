6d73271ca80f049c1d9e7054347e66a325765282
Ovais Tariq <ot@uber.com>
2018-02-14 21:37:04 -0800

    Add support for sysbench 1.0. Use sysbench hooks to generate CSV output.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# benchmark_automation
-Scripts to help automate the running of repeatable benchmarks
+# Benchmark Automation
+Scripts to help automate the running of repeatable benchmarks. Currently only
+sysbench is supported.
+
+## Dependencies
+* sysbench 1.0.12-1 or higher - [installtion
+   instructions](https://github.com/akopytov/sysbench#installing-from-binary-packages).
+
--- benchmark_wrappers/run_sysbench.sh
+++ benchmark_wrappers/run_sysbench.sh
@@ -6,18 +6,22 @@
 cat <<EOF>&2
 
    usage: [env var1=val1 var2=val2 ...] $0
-   No direct input args, behavior is controlled by the following environment variables: 
+   No direct input args, behavior is controlled by the following environment
+   variables: 
      - _EXP_NAME : Experiment name. Optional, defaults to 'sysbench'
-     - _TESTS : Quoted list of the tests to run, i.e. "oltp update_index"
-     - _TESTS_PATH : The path where to look for the lua scripts defining the tests. Optional, no default value. Must include trailing '/'
-     - _THREADS : Quoted list of the # of threads to use for each run, i.e. "16 32 64"
-     - _SIZE : Quoted list of the table sizes to use, in rows, i.e. "100000 1000000 10000000" 
+     - _TESTS : Quoted list of the tests to run, i.e. "oltp_read_only
+       oltp_read_write".
+     - _THREADS : Quoted list of the # of threads to use for each run, i.e.
+       "16 32 64"
+     - _SIZE : Quoted list of the table sizes to use, in rows, i.e.
+       "100000 1000000 10000000"
    
-   Any actual input argument will be passed as is to sysbench, so you can run this like so: 
+   Any actual input argument will be passed as is to sysbench, so you can run
+   this like so:
 
-   env _TESTS="oltp update_index" _THREADS="16 32" _SIZE="1000 10000" $0 --rand-type=pareto --rand-init=on --report-interval=10 --mysql-host=sbhost --mysql-db=sbtest --max-time=7200 --max-requests=0
+   env _TESTS="oltp_read_only oltp_read_write" _THREADS="16 32" _SIZE="1000 10000" $0 --rand-type=pareto --rand-init=on --report-interval=10 --mysql-host=sbhost --mysql-db=sbtest --max-time=7200 --max-requests=0
 
-   env _TESTS_PATH=/usr/share/doc/sysbench/tests/db/ _EXP_NAME=sample _TESTS="oltp update_index" _THREADS="1 2 4" _SIZE="10 100" ./run_sysbench.sh --oltp_tables_count=2 --mysql-user=sysbench --mysql-password=sysbench --mysql_table_engine=innodb --rand-type=pareto --rand-init=on --report-interval=10 --mysql-db=sbtest --max-time=5 --max-requests=0 
+   env _EXP_NAME=sample _TESTS="oltp_read_only oltp_read_write" _THREADS="1 2 4" _SIZE="10 100" ./run_sysbench.sh --oltp_tables_count=2 --mysql-user=sysbench --mysql-password=sysbench --mysql_table_engine=innodb --rand-type=pareto --rand-init=on --report-interval=10 --mysql-db=sbtest --max-time=5 --max-requests=0 
 
 
 EOF
@@ -28,15 +32,25 @@ exit 1
 
 [ -z "$_EXP_NAME" ] && _EXP_NAME="sysbench"
 
+SCRIPT_ROOT=$(dirname $(readlink -f $0))
+TEST_DIR=${SCRIPT_ROOT}/../sysbench_tests
+
 for test in $_TESTS; do
+    test_path=${TEST_DIR}/${test}.lua
+
+    if [[ ! -f ${test_path} ]]; then
+        echo "Skipping test ${test}, as it is not yet supported"
+        continue
+    fi
+
     mkdir $test 2>/dev/null #ignore if it exists
     pushd $test
     for threads in $_THREADS; do
 	for size in $_SIZE; do
 	    echo "Starting sysbench for test=$test, threads=$threads, size=$size" 
-	    sysbench --test=$_TESTS_PATH$test.lua --num-threads=$threads --oltp-table-size=$size $* prepare
-	    sysbench --test=$_TESTS_PATH$test.lua --num-threads=$threads --oltp-table-size=$size $* run | tee $_EXP_NAME.thr$threads.sz$size.test$test.txt 
-	    sysbench --test=$_TESTS_PATH$test.lua --num-threads=$threads --oltp-table-size=$size $* cleanup
+	    sysbench ${test_path} --threads=$threads --table-size=$size $* prepare
+	    sysbench ${test_path} --threads=$threads --table-size=$size $* run | tee $_EXP_NAME.thr$threads.sz$size.test$test.txt 
+	    sysbench ${test_path} --threads=$threads --table-size=$size $* cleanup
         done
     done
     popd
