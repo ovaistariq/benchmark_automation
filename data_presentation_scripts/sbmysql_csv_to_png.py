107c7894dc1af9814c08f60eb3f2aec30f90b1f6
Ovais Tariq <ot@uber.com>
2018-02-16 15:41:03 -0800

    Graph generation script now supports generating multiple graphs with multiple series

--- data_presentation_scripts/sbmysql_csv_to_png.py
+++ data_presentation_scripts/sbmysql_csv_to_png.py
@@ -9,11 +9,24 @@ from matplotlib import style
 matplotlib.use('Agg')
 style.use('ggplot')
 
+# Importing the plotting library after setting the backend otherwise it tries to load with default backend and fails
 import matplotlib.pyplot as plt
 
 
 # The columns in the CSV file corresponding to the values
-COLS = ['ts', 'threads', 'tps', 'qps', 'reads', 'writes', 'other', 'response_time', 'errors', 'reconnects']
+CSV_FIELDS = ['ts', 'threads', 'tps', 'qps', 'reads', 'writes', 'other', 'response_time', 'errors', 'reconnects']
+
+# The columns that can be plotted on the graphs
+GRAPH_FIELDS = ['tps', 'qps', 'reads', 'writes', 'response_time']
+
+# The labels of the co
+GRAPH_FIELD_LABELS = {
+    'tps': {'xlabel': 'time (s)', 'ylabel': 'Transactions/second', 'title': 'MySQL TPS'},
+    'qps': {'xlabel': 'time (s)', 'ylabel': 'Queries/second', 'title': 'MySQL QPS'},
+    'reads': {'xlabel': 'time (s)', 'ylabel': 'Reads/second', 'title': 'MySQL RPS'},
+    'writes': {'xlabel': 'time (s)', 'ylabel': 'Writes/second', 'title': 'MySQL WPS'},
+    'response_time': {'xlabel': 'time (s)', 'ylabel': 'P99 Latency (ms)', 'title': 'MySQL P99 Latency (ms)'}
+}
 
 
 @click.group(invoke_without_command=True)
@@ -25,38 +38,80 @@ def cli(ctx):
 
 
 @cli.command()
+@click.pass_context
+@click.option('--output', '-o', default='output', help='The directory where to output the graphs. Any existing file '
+                                                       'would be overwritten')
+@click.option('--plot-fields', '-c', default=['tps', 'qps', 'response_time'], multiple=True,
+              help='The fields in the CSV file for which to plot the graphs. '
+                   'Supported fields are: %s' % ', '.join(GRAPH_FIELDS))
 @click.argument('csv_files', type=click.Path(exists=True, resolve_path=True), nargs=-1)
-def plot_graph(csv_files):
+def plot_graph(ctx, output, plot_fields, csv_files):
     if len(csv_files) < 1:
+        print(ctx.get_help())
         exit(-1)
 
-    # Initialize the plot
-    fig, ax = plt.subplots()
+    if len(plot_fields) < 1:
+        print(ctx.get_help())
+        exit(-1)
+
+    # Setup the plotting data for each of the files provided as well as each of the supported graph fields
+    plot_data = {}
+    for col in GRAPH_FIELDS:
+        if col not in plot_fields:
+            continue
+
+        plot_data[col] = {
+            'series': {}
+        }
 
     for file_path in csv_files:
         filename = os.path.basename(file_path)
         name_parts = filename.split('.')
 
-        ts = [0]
-        tps = [0]
-        response_time = [0]
+        # The first part of the filename is the series name for the purpose of the graph
+        series_name = name_parts[0]
+
         with open(file_path) as f:
-            reader = csv.DictReader(f, COLS)
+            reader = csv.DictReader(f, CSV_FIELDS)
             for row in reader:
-                ts.append(row['ts'])
-                tps.append(row['tps'])
-                response_time.append(row['response_time'])
+                for col, val in row.iteritems():
+                    # We ignore the columns that are not to be plotted
+                    if col not in plot_fields:
+                        continue
 
-        ax.plot(ts, tps, linewidth=2, alpha=0.6, label=name_parts[0])
+                    # Initialize the series data if the series is seen for the first time for a particular column
+                    if series_name not in plot_data[col]['series']:
+                        plot_data[col]['series'][series_name] = {
+                            'x': [],
+                            'y': [],
+                            'label': ''
+                        }
 
-    ax.set(xlabel='time (s)', ylabel='Transactions/second', title='MySQL TPS')
-    ax.grid()
-    ax.legend(loc='upper right')
+                    plot_data[col]['series'][series_name]['x'].append(float(row['ts']))
+                    plot_data[col]['series'][series_name]['y'].append(float(val))
+                    plot_data[col]['series'][series_name]['label'] = series_name
 
-    plt.tight_layout()
+    # Setup the output directory
+    if not os.path.exists(output):
+        os.makedirs(output)
 
-    fig.savefig("test.png")
-    plt.close()
+    # Plot and save the graphs
+    for col_name, col_data in plot_data.iteritems():
+        # Initialize the plot
+        fig, ax = plt.subplots()
+
+        for _, series in col_data['series'].iteritems():
+            ax.plot(series['x'], series['y'], linewidth=2, alpha=0.6, label=series['label'])
+
+        ax.set(xlabel=GRAPH_FIELD_LABELS[col_name]['xlabel'], ylabel=GRAPH_FIELD_LABELS[col_name]['ylabel'],
+               title=GRAPH_FIELD_LABELS[col_name]['title'])
+        ax.grid()
+        ax.legend(loc='best')
+
+        plt.tight_layout()
+
+        fig.savefig(os.path.join(output, '%s.png' % col_name))
+        plt.close()
 
 
 if __name__ == '__main__':
