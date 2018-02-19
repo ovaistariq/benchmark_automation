7546e817f4aceeb5336d2af7a9af23bb4454c625
Ovais Tariq <ot@uber.com>
2018-02-18 23:39:58 -0800

    Add more configuration options for IO benchmark

--- benchmark_wrappers/run_sbio.sh
+++ benchmark_wrappers/run_sbio.sh
@@ -1,10 +1,12 @@
-$ cat sysbench.sh
 #!/bin/bash
 
 # variables
+TEST_MODE=${TEST_MODE:-rndrd rndwr}
 FILE_TOTAL_SIZE=${FILE_TOTAL_SIZE:-600G}
 IO_REQUEST_SIZE=${IO_REQUEST_SIZE:-16K}
 FSYNC_FREQ=${FSYNC_FREQ:-0}
+IO_MODE=${IO_MODE:-async}
+FILE_FLAG=${FILE_FLAG:-direct}
 BENCH_RUN_TIME=${BENCH_RUN_TIME:-300}
 THREADS=${THREADS:-16}
 EXP_NAME=${EXP_NAME:-sysbench}
@@ -12,29 +14,27 @@ EXP_NAME=${EXP_NAME:-sysbench}
 # helper functions
 prepare_benchmark() {
     echo "-- Preparing dataset of size $FILE_TOTAL_SIZE"
-    sysbench --test=fileio --file-num=64 \
-        --file-total-size=${FILE_TOTAL_SIZE} prepare
+    sysbench --test=fileio --file-num=64 --file-total-size="${FILE_TOTAL_SIZE}" prepare
 }
 
 cleanup_benchmark() {
     echo "-- Cleaning up dataset of size $FILE_TOTAL_SIZE"
-    sysbench --test=fileio --file-num=64 \
-        --file-total-size=${FILE_TOTAL_SIZE} cleanup
+    sysbench --test=fileio --file-num=64 --file-total-size="${FILE_TOTAL_SIZE}" cleanup
 }
 
 run_benchmark() {
     for thd in $THREADS; do
-        for mode in rndrd rndwr; do
+        for mode in ${TEST_MODE}; do
             echo "-- Running benchmark threads: $thd, mode: $mode"
             sysbench --test=fileio --file-num=64 \
-                --file-total-size=${FILE_TOTAL_SIZE} \
-                --file-test-mode=${mode} --file-io-mode=async \
-                --file-extra-flags=direct \
-                --file-fsync-freq=${FSYNC_FREQ} \
-                --file-block-size=${IO_REQUEST_SIZE} \
-                --time=${BENCH_RUN_TIME} \
-                --threads=${thd} --report-interval=10 --percentile=99 run \
-            | tee $EXP_NAME.thr.$thd.sz.$FILE_TOTAL_SIZE.test.$mode.txt
+                --file-total-size="${FILE_TOTAL_SIZE}" \
+                --file-test-mode="${mode}" --file-io-mode="${IO_MODE}" \
+                --file-extra-flags="${FILE_FLAG}" \
+                --file-fsync-freq="${FSYNC_FREQ}" \
+                --file-block-size="${IO_REQUEST_SIZE}" \
+                --time="${BENCH_RUN_TIME}" \
+                --threads="${thd}" --report-interval=10 --percentile=99 run \
+            | tee "$EXP_NAME.thr.$thd.sz.$FILE_TOTAL_SIZE.test.$mode.txt"
         done
     done
 }
