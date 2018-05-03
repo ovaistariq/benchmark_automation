3d3b38d5411738e79d3ee69860afb6f37ae52eab
Ovais Tariq <ot@uber.com>
2018-05-03 16:21:43 -0700

    Fix conversion of mysqladmin output to csv format

--- data_collection_scripts/collect-mysqladmin.sh
+++ data_collection_scripts/collect-mysqladmin.sh
@@ -14,19 +14,18 @@ output_prefix=$3
 dest="${output_prefix}.${TOOLNAME}_$(ts).gz"
 trap "rm -f $dest.pid" SIGINT SIGTERM SIGHUP
 # get the header. -r should not be needed here, but just in case the output columns are ever different based on that option
-$TOOLNAME ext -i 1 -c 1 -r|grep -v '-'|awk -F '|' '{print $2}'|grep -v ^$|sed 's/Variable_name/Timestamp/g'|tr '\n' ','|\
-  sed 's/  */ /g
-       s/^ //
-       s/,$/\n/' > $dest.header
+$TOOLNAME ext -i 1 -c 1 -r | grep -v Variable_name | grep "|" | awk -F '|' '{gsub(/ /, "", $2); print $2}' | tr '\n' ',' > $dest.header
+
 # now get the capture. since we're using -r, discard the first row 
-read_first_row=0
-ts=1
 arg_duration=""
 [ -n "$duration" -a $duration -gt 0 ] && arg_duration="-c $(( $duration/$interval ))"
-$TOOLNAME ext -i $interval $arg_duration -r | awk -F '|' '{print $3}'|sed 's/^ //g'|sed 's/  *$/ /g'|grep -v ^$|sed 's/^ $/_/g' |tr '\n' ','| sed "s/Value/|/g" | tr '|' '\n' |grep -v ^$|\
-  sed 's/^  *//g
-  s/  */ /g
-  s/,$//'| while read row; do [ $read_first_row -gt 0 ] && echo "$ts $row"; ts=$((ts+1)); read_first_row=1; done | gzip -c >> $dest & 
+
+read_first_row=0
+
+$TOOLNAME ext -i $interval $arg_duration -r | grep -v Variable_name \
+    | grep "|" | awk -F '|' '{gsub(/ /, "", $3); print $3}' | tr '\n' ',' \
+    | while read row; do [ $read_first_row -gt 0 ] && echo "$row"; read_first_row=1; done | gzip -c >> $dest &
+
 echo $! > $dest.pid
 pid=$!
 # save the pid so we can monitor disk space while the tool runs, and
